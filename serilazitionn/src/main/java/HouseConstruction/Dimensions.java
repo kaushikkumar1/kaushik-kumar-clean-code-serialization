0f799d4af25199de955bac8db24a9f74c2d53d80
kaushik <kaushikkumar004@gmail.com>
2020-02-25 19:59:41 +0530

    Kaushik commited to ....

---
+++ serilazitionn/src/main/java/HouseConstruction/Dimensions.java
@@ -0,0 +1,16 @@
+package HouseConstruction;
+
+
+public class Dimensions {
+  
+	  Integer length,breadth;
+	 
+	  public Dimensions(){
+		  
+	  }
+	  public Dimensions(Integer length, Integer breadth){
+		this.length=length;
+		this.breadth=breadth;
+	  }
+	  
+}
